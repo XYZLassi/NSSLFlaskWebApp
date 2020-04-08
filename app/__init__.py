b264a1374cdc5d34243664a35296d136198e2145
Lassi <csharplassi@posteo.de>
2020-04-08 11:07:47 +0200

    Add: Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ import os
 
 from flask import Flask
 
+from flask_login import LoginManager
+
+login = LoginManager()
+
 
 def create_app(config=None, load_views=None):
     if config is None:
@@ -17,9 +21,17 @@ def create_app(config=None, load_views=None):
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
 
+    login.init_app(app)
+
     load_views = app.config['LOAD_VIEWS'] if load_views is None else load_views
     if load_views:
         from .routes.main import bp as bp_main
         app.register_blueprint(bp_main)
 
     return app
+
+
+@login.user_loader
+def user_loader(id):
+    from .models.user import User
+    return User()

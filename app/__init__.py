43812a397e78dd70d040d60892cf0b62e658bc01
Lassi <csharplassi@posteo.de>
2020-04-08 19:31:38 +0200

    Add: Autologin for User If Anonymous

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,12 @@ import importlib
 import os
 
 from flask import Flask
-
 from flask_login import LoginManager
 
 login = LoginManager()
 
 
-def create_app(config=None, load_views=None):
+def create_app(config=None, load_views=None) -> Flask:
     if config is None:
         module = importlib.import_module('config')
         app_settings = os.environ.get('APP_SETTINGS') or 'ProductionConfig'
@@ -23,6 +22,22 @@ def create_app(config=None, load_views=None):
 
     login.init_app(app)
     login.login_view = "main.login"
+
+    # AutoLogin for Debug
+    if app.config['DEBUG'] and app.config['TEST_AUTO_LOGIN']:
+        def get_default_user():
+            from nssl import NSSL
+            from .models.user import User
+            from .context import UserStorage
+
+            nssl = NSSL(app.config['NSSL_SERVER_URL'])
+            response = nssl.login(app.config['AUTO_LOGIN_USER'],
+                                  app.config['AUTO_LOGIN_PASSWORD'])
+            user = User(user_data=response.data)
+            UserStorage.add(user.user_id, user)
+            return user
+
+        login.anonymous_user = get_default_user
 
     load_views = app.config['LOAD_VIEWS'] if load_views is None else load_views
     if load_views:
--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     DEBUG = False
     TESTING = False
 
+    TEST_AUTO_LOGIN = False
+
     LOAD_VIEWS = os.environ.get('LOAD_VIEWS', True)
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
     NSSL_SERVER_URL = os.environ.get('NSSL_SERVER_URL')
@@ -17,6 +19,11 @@ class ProductionConfig(Config):
 class DevelopConfig(Config):
     DEBUG = True
 
+    TEST_AUTO_LOGIN = os.environ.get('TEST_AUTO_LOGIN', False)
+    AUTO_LOGIN_USER = os.environ.get('AUTO_LOGIN_USER', None)
+    AUTO_LOGIN_PASSWORD = os.environ.get('AUTO_LOGIN_PASSWORD', None)
 
-class TestConfig(Config):
+
+class TestConfig(DevelopConfig):
     TESTING = True
+    DEBUG = False

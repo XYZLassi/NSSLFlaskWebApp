31090d9e58e64ebc52d6594d44a61a92d45a4eef
Lassi <csharplassi@posteo.de>
2020-04-08 11:42:14 +0200

    Add: Simple Login Page

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,3 +6,14 @@ bp = flask.Blueprint('main', __name__)
 @bp.route('/', methods=['GET'])
 def index():
     return flask.render_template('main/index.html')
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    from ..forms.user import LoginForm
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        return flask.redirect(flask.url_for('main.index'))
+
+    return flask.render_template('main/login.html', title='Login', form=form)
